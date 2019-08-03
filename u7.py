5f95c8aef62c90e35ebec40f7114377d3979180c
Luc AiYu <aiyu2009@126.com>
2019-08-03 20:58:36 +0800

    20190803

---
+++ u7.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+def number(number):
+    if number%2==0:
+        print('%d is ou shu' % number)
+    else:
+        print('%d is ji shu' % number)
+
+number(1)
+number(2)
+number(9999999999999999999999999999999999999999999999999999999999999999999999999999999999999)
+number(-2)
+
+
